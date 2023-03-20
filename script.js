525edf0ea173f3fc0439a1fd979d27195e8b8830
Guilherme Elias <guieliasmonteiro@gmail.com>
2023-03-20 08:45:38 -0300

    Atualização: Mensagem do input

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const passwordInput = document.getElementById("password");
 const passwordRegex = /^[A-Za-z0-9]\w{8,}$/;
 const emailInput = document.getElementById("email");
 const emailRegex = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm;
+const loginWidth = document.getElementsByClassName('login-with');
 
 
 loginBtn.addEventListener("click", (e) => {
@@ -11,16 +12,23 @@ loginBtn.addEventListener("click", (e) => {
   
     if(passwordRegex.test(passwordInput.value) == true && emailRegex.test(emailInput.value) == true) {
         alert("Logado");
+
     }
     else if(passwordRegex.test(passwordInput.value) == false && emailRegex.test(emailInput.value) == false) {
         passwordInput.style.border = "2px solid red";
-        emailInput.style.border = "2px solid red";  
+        emailInput.style.border = "2px solid red";
+
+        document.getElementById('msg-password').innerHTML = `*Digite sua senha!`
+        document.getElementById('msg-email').innerHTML = `*Digite seu email!`
+
     }
     else if (passwordRegex.test(passwordInput.value) == false) {
         passwordInput.style.border = "2px solid red";
+        document.getElementById('msg-password').innerHTML = `*Digite sua senha correta!`
     }
     else if(emailRegex.test(emailInput.value) == false){
-        emailInput.style.border = "2px solid red";    
+        emailInput.style.border = "2px solid red";
+        document.getElementById('msg-email').innerHTML = `*Digite seu E-mail corretamente!`
     } 
 
     console.log(emailRegex.test(emailInput.value));
@@ -29,8 +37,13 @@ loginBtn.addEventListener("click", (e) => {
 
 emailInput.addEventListener("click", () => {
     emailInput.style.border = "none"
+    emailInput.value = ""
+    document.getElementById('msg-email').innerHTML = ""
+    document.getElementById('msg-password').innerHTML = ""
 });
 
 passwordInput.addEventListener("click", () => {
     passwordInput.style.border = "none"
+    passwordInput.value = ""
+    document.getElementById('msg-password').innerHTML = ""
 })
